f454f5b1e353cdddd16e967e1ecc5db665b02931
maximusson <64659505+maximusson@users.noreply.github.com>
2020-12-06 16:48:07 +0100

    Update task2.js

--- 2020/day-06-custom-customs/task2.js
+++ 2020/day-06-custom-customs/task2.js
@@ -1,13 +1,27 @@
 const fs = require('fs')
-
 const input = fs.readFileSync('input.txt').toString();
 
-// dict, key: char, value: 
 const uniques = {};
 Array.from(new Set(input.split("\n").join("").split(""))).forEach(unique => uniques[unique] = 0);
-// { a: 0, b: 0, c: 0 }
 
 const groups = input.split("\n\n").map(group => [group]).map(user => user[0].split("\n"));
+ 
+groups.forEach(group => {
+    Object.keys(uniques).forEach(key => {
+        sameAnswer = true;
+        group.forEach(answer => {
+            if (!answer.includes(key)) sameAnswer = false;
+        })
+        if (sameAnswer) uniques[key]++;
+    });
+});
+
+console.log(Object.values(uniques).reduce((acc, cur) => acc + cur));
+
+
+// uniques - dict, key: char, value: 
+// { a: 0, b: 0, c: 0 }
+
 // groups -> group -> answers
 // [
 //     [ 'abc' ],
@@ -15,21 +29,4 @@ const groups = input.split("\n\n").map(group => [group]).map(user => user[0].spl
 //     [ 'ab', 'ac' ],
 //     [ 'a', 'a', 'a', 'a' ],
 //     [ 'b' ]
-// ]   
-
-groups.forEach(group => {
-    Object.keys(uniques).forEach(key => {
-        sameAnswer = true;
-        group.forEach(answer => {
-            if (!answer.includes(key)) {
-                sameAnswer = false
-            }
-        })
-        if (sameAnswer) {
-            uniques[key]++;
-        }
-    });
-});
-
-const count = Object.values(uniques).reduce((acc, cur) => acc + cur);
-console.log(count)
+// ]  
